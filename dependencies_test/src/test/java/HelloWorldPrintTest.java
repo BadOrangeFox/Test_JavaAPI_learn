ab28b3dbafe7f7b1d29f9fcb5e2b5622e1825056
mosco <erofeeviaa@pik.ru>
2022-12-30 15:09:12 +0300

    TestHellogit

---
+++ dependencies_test/src/test/java/HelloWorldPrintTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldPrintTest {
+    @Test
+    public void testHelloWorldPrintTest(){
+        System.out.println("Hello from Yaroslav");
+    }
+}
